1acd492f8fb875297bc4227fbfee2703ad6cd950
Phat tran <ngocphxt2018@gmail.com>
2023-11-09 02:19:28 +0700

    2pm

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Component } from 'react';
+import firebase from 'firebase/app'
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+import LandingScreen from './components/auth/Landing'
+import RegisterScreen, { Register } from './components/auth/Register'
+import { getApps, initializeApp } from 'firebase/app';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDLgC2RgF6pjsSOgdi4dATo95Qtmw1cRf8",
+  authDomain: "social-app-b7cc6.firebaseapp.com",
+  projectId: "social-app-b7cc6",
+  storageBucket: "social-app-b7cc6.appspot.com",
+  messagingSenderId: "37162901414",
+  appId: "1:37162901414:web:7edd82771264e681643967",
+  measurementId: "G-GYCFKT2E4B"
+};
+
+if(getApps().length === 0) {
+  initializeApp(firebaseConfig)
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createStackNavigator();
+export default function App() {
+  return (
+    <NavigationContainer>
+        <Stack.Navigator initialRouteName = "Landing" >
+          <Stack.Screen name = "Landing" component = {LandingScreen} options = {{headerShown: false}}/>
+          <Stack.Screen name = "Register" component = {Register}/>
+        </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ components/auth/Register.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { Component } from 'react';
+import {View, Button, TextInput} from 'react-native'
+import firebase from 'firebase/app'
+import 'firebase/auth';
+import { initializeApp } from 'firebase/app';
+
+export class Register extends Component {
+constructor(props) {
+    super(props);
+
+    this.state = {
+        email : '',
+        password: '',
+        name: ''
+    }
+    this.onSignUp = this.onSignUp.bind(this)
+}
+onSignUp() {
+    const { email, password, name} = this.state;
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+    .then((result) => {
+        console.log(result)
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+}
+    render() {
+        return (
+            <View>
+                <TextInput 
+                placeholder="Name"
+                onChangeText={(name) => this.setState({name })}
+                
+                />
+                <TextInput 
+                placeholder="Email"
+                onChangeText={(email) => this.setState({email })}
+                
+                />
+                <TextInput 
+                placeholder="Password"
+                secureTextEntry={true}
+                onChangeText={(password) => this.setState({password })}
+                
+                />
+                <Button 
+                    onPress={() => this.onSignUp() }
+                    title="Sign up"
+                />
+
+               
+            </View>
+        )
+    }
+} 
+
+export default Register
